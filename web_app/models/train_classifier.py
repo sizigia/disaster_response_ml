b881ad0b4650262ebdf1642bf89bca43b24825f9
sizigia <faustina.giaquinta@gmail.com>
2020-04-28 14:07:03 -0400

    update training pipeline

--- web_app/models/train_classifier.py
+++ web_app/models/train_classifier.py
@@ -1,9 +1,13 @@
+
 import sys
 import nltk
-#nltk.download(['punkt', 'wordnet'])
-from nltk.tokenize import word_tokenize
+nltk.download(['averaged_perceptron_tagger', 'wordnet'])
+from nltk import pos_tag
+from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
+import numpy as np
 import pandas as pd
 import pickle
 import re
@@ -12,10 +16,8 @@ from sqlalchemy import create_engine
 
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
-from sklearn.model_selection import GridSearchCV
 from sklearn.multioutput import MultiOutputClassifier
 
 
@@ -31,47 +33,97 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
-    tokens = word_tokenize(text)
+    """
+    Transforms a text to clean tokens, where every token is a word converted to lower case,
+    passed to a part-of-speech tagger and lemmatized accordingly.
+    Words recognized as stopwords are ommitted.
+    
+    Input:
+        text (str)
+        
+    Output:
+        clean_tokens (list): list of clean tokens (words converted to lower case and lemmatized)
+        
+    """
+    
+    tokenizer = RegexpTokenizer('\w+')
     lemmatizer = WordNetLemmatizer()
+
+    tokens = tokenizer.tokenize(text.lower())
     
     clean_tokens = []
     
-    for tok in tokens:
-        clean_token = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_token)
+    for word, tag in pos_tag(tokens):
+        if tag[0] in ['A', 'R', 'N', 'V']:
+            tag = tag[0].lower()
+            clean_token = lemmatizer.lemmatize(word, pos=tag)
+        else:
+            clean_token = word
+            
+        if clean_token not in stopwords.words('english'):
+            clean_tokens.append(clean_token)
         
     return clean_tokens
 
 
 def build_model():
+    """
+    No input needed. Returns a pipeline with the next steps:
+        1. vect - Converts a collection of text documents to a matrix of token counts
+        2. tfidf - Transforms a count matrix to a normalized *term-frequency* or 
+                  *term-frequency times inverse document-frequency* representation
+        3. clf - Multi target random forest classification. It reuses the solution 
+                    of the previous call to fit and add more estimators to the ensemble.    
+    """
+    
     model = Pipeline([
-    ('vect', CountVectorizer(tokenizer=tokenize)),
-    ('clf', MultiOutputClassifier(AdaBoostClassifier(n_estimators=100, 
-                                                     learning_rate=0.5)))
-    ])
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(warm_start=True))),
+    ], verbose=True)
     
-    parameters = {
-    'clf__estimator__learning_rate': [0.1, 0.2, 0.5],
-    'clf__estimator__n_estimators': [100, 200, 300]
-    }
-
-    cv = GridSearchCV(model, parameters)
-    
-    return cv
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    y_pred = model.predict(X_test)
+    """
+    Takes the model, X and Y test set and category names, and returns precision, recall and F1 score 
+    for every feature in the dataset, and the overall accuracy of the model.
+    
+    Input:
+        model ():
+        X_test (pandas.core.series.Series): a subset of Y with the purpose of testing the model
+        Y_test (pandas.core.series.Series): predictions made with X_test by the model
+        category_names (): 
+        
+    Output:
+        Prints out the following format
+            feature_name
+                Precision: __%
+                Recall: __%
+                F1 Score: __%
+                
+                ...
+                
+                Accuracy Score: __%
+                
+        And also returns the full value of accuracy.
+    """
+    
+    Y_pred = model.predict(X_test)
     
     for idx, col in enumerate(category_names):
-        set_y_pair = (y_test[col], y_pred[:, idx])
+        set_Y_pair = (Y_test[col], Y_pred[:, idx])
         avg='weighted'
-        rep_col = "{}\n\tPrecision: {:.2f}%\n\tRecall: {:.2f}%\n\tF1 Score: {:.2f}%\n\tAccuracy: {:.2f}%".format(col,
-                                                                                 precision_score(*set_y_pair, average=avg), 
-                                                                                 recall_score(*set_y_pair, average=avg), 
-                                                                                 f1_score(*set_y_pair, average=avg))
-    print(rep_col)
-    print('Accuracy Score: {}'.format(np.mean(Y_test.values == y_pred)))
+        rep_col = "{}\n\tPrecision: {:.2f}%\n\tRecall: {:.2f}%\n\tF1 Score: {:.2f}%\n".format(col,
+                                                                                 precision_score(*set_Y_pair, average=avg), 
+                                                                                 recall_score(*set_Y_pair, average=avg), 
+                                                                                 f1_score(*set_Y_pair, average=avg))
+        print(rep_col)
+        
+    print('Accuracy Score: {:.2f}%'.format(np.mean(Y_pred.values == Y_pred)))
+
+    return np.mean(Y_pred.values == Y_pred)
 
 
 def save_model(model, model_filepath):
